9c8cbca515a29b99f9f75d7776733665f1fccbec
hisway <hesiway@gmail.com>
2019-07-13 22:24:50 +0800

    taobao.tbk.sc.shop.convert

--- src/top/request/TbkScCouponBrandRecommendRequest.php
+++ src/top/request/TbkScCouponBrandRecommendRequest.php
@@ -1,132 +1,135 @@
 <?php
-namespace TopClient\request;
-
-use TopClient\RequestCheckUtil;
 /**
  * TOP API: taobao.tbk.sc.coupon.brand.recommend request
- * 
+ *
  * @author auto create
  * @since 1.0, 2018.11.08
  */
+
+namespace TopClient\request;
+
+use TopClient\RequestCheckUtil;
+
 class TbkScCouponBrandRecommendRequest
 {
-	/** 
-	 * mm_xxx_xxx_xxx的第三位
-	 **/
-	private $adzoneId;
-	
-	/** 
-	 * 频道，支持的值：zonghe	综合 neiyi	内衣 nvzhuang	女装 muying	母婴 nanzhuang	男装 shipin	食品 jiajujiazhuang	家居家装 shumajiadian	数码家电 meizhuanggehu	美妆个护 yundonghuwai	运动户外 xiebaopeishi	鞋包配饰
-	 **/
-	private $channel;
-	
-	/** 
-	 * 第几页，默认：1（最多出200个结果）
-	 **/
-	private $pageNo;
-	
-	/** 
-	 * 页大小，默认20，1~100
-	 **/
-	private $pageSize;
-	
-	/** 
-	 * 1：PC，2：无线，默认：1
-	 **/
-	private $platform;
-	
-	/** 
-	 * mm_xxx_xxx_xxx的第二位
-	 **/
-	private $siteId;
-	
-	private $apiParas = array();
-	
-	public function setAdzoneId($adzoneId)
-	{
-		$this->adzoneId = $adzoneId;
-		$this->apiParas["adzone_id"] = $adzoneId;
-	}
+    /**
+     * mm_xxx_xxx_xxx的第三位
+     **/
+    private $adzoneId;
 
-	public function getAdzoneId()
-	{
-		return $this->adzoneId;
-	}
+    /**
+     * 频道，支持的值：zonghe    综合 neiyi    内衣 nvzhuang    女装 muying    母婴 nanzhuang    男装 shipin    食品 jiajujiazhuang    家居家装 shumajiadian    数码家电 meizhuanggehu    美妆个护 yundonghuwai    运动户外 xiebaopeishi    鞋包配饰
+     **/
+    private $channel;
 
-	public function setChannel($channel)
-	{
-		$this->channel = $channel;
-		$this->apiParas["channel"] = $channel;
-	}
+    /**
+     * 第几页，默认：1（最多出200个结果）
+     **/
+    private $pageNo;
 
-	public function getChannel()
-	{
-		return $this->channel;
-	}
+    /**
+     * 页大小，默认20，1~100
+     **/
+    private $pageSize;
 
-	public function setPageNo($pageNo)
-	{
-		$this->pageNo = $pageNo;
-		$this->apiParas["page_no"] = $pageNo;
-	}
+    /**
+     * 1：PC，2：无线，默认：1
+     **/
+    private $platform;
 
-	public function getPageNo()
-	{
-		return $this->pageNo;
-	}
+    /**
+     * mm_xxx_xxx_xxx的第二位
+     **/
+    private $siteId;
 
-	public function setPageSize($pageSize)
-	{
-		$this->pageSize = $pageSize;
-		$this->apiParas["page_size"] = $pageSize;
-	}
+    private $apiParas = array();
 
-	public function getPageSize()
-	{
-		return $this->pageSize;
-	}
+    public function setAdzoneId($adzoneId)
+    {
+        $this->adzoneId = $adzoneId;
+        $this->apiParas["adzone_id"] = $adzoneId;
+    }
 
-	public function setPlatform($platform)
-	{
-		$this->platform = $platform;
-		$this->apiParas["platform"] = $platform;
-	}
+    public function getAdzoneId()
+    {
+        return $this->adzoneId;
+    }
 
-	public function getPlatform()
-	{
-		return $this->platform;
-	}
+    public function setChannel($channel)
+    {
+        $this->channel = $channel;
+        $this->apiParas["channel"] = $channel;
+    }
 
-	public function setSiteId($siteId)
-	{
-		$this->siteId = $siteId;
-		$this->apiParas["site_id"] = $siteId;
-	}
+    public function getChannel()
+    {
+        return $this->channel;
+    }
 
-	public function getSiteId()
-	{
-		return $this->siteId;
-	}
+    public function setPageNo($pageNo)
+    {
+        $this->pageNo = $pageNo;
+        $this->apiParas["page_no"] = $pageNo;
+    }
 
-	public function getApiMethodName()
-	{
-		return "taobao.tbk.sc.coupon.brand.recommend";
-	}
-	
-	public function getApiParas()
-	{
-		return $this->apiParas;
-	}
-	
-	public function check()
-	{
-		
-		RequestCheckUtil::checkNotNull($this->adzoneId,"adzoneId");
-		RequestCheckUtil::checkNotNull($this->siteId,"siteId");
-	}
-	
-	public function putOtherTextParam($key, $value) {
-		$this->apiParas[$key] = $value;
-		$this->$key = $value;
-	}
+    public function getPageNo()
+    {
+        return $this->pageNo;
+    }
+
+    public function setPageSize($pageSize)
+    {
+        $this->pageSize = $pageSize;
+        $this->apiParas["page_size"] = $pageSize;
+    }
+
+    public function getPageSize()
+    {
+        return $this->pageSize;
+    }
+
+    public function setPlatform($platform)
+    {
+        $this->platform = $platform;
+        $this->apiParas["platform"] = $platform;
+    }
+
+    public function getPlatform()
+    {
+        return $this->platform;
+    }
+
+    public function setSiteId($siteId)
+    {
+        $this->siteId = $siteId;
+        $this->apiParas["site_id"] = $siteId;
+    }
+
+    public function getSiteId()
+    {
+        return $this->siteId;
+    }
+
+    public function getApiMethodName()
+    {
+        return "taobao.tbk.sc.coupon.brand.recommend";
+    }
+
+    public function getApiParas()
+    {
+        return $this->apiParas;
+    }
+
+    public function check()
+    {
+
+        RequestCheckUtil::checkNotNull($this->adzoneId, "adzoneId");
+        RequestCheckUtil::checkNotNull($this->siteId, "siteId");
+    }
+
+    public function putOtherTextParam($key, $value)
+    {
+        $this->apiParas[$key] = $value;
+        $this->$key = $value;
+    }
 }
--- src/top/request/TbkScMaterialOptionalRequest.php
+++ src/top/request/TbkScMaterialOptionalRequest.php
@@ -1,517 +1,520 @@
 <?php
+/**
+ * TOP API: taobao.tbk.sc.material.optional request
+ *
+ * @author auto create
+ * @since 1.0, 2019.07.04
+ */
+
 namespace TopClient\request;
 
 use TopClient\RequestCheckUtil;
-/**
- * TOP API: taobao.tbk.sc.material.optional request
- * 
- * @author auto create
- * @since 1.0, 2018.11.10
- */
+
 class TbkScMaterialOptionalRequest
 {
-	/** 
-	 * adzone_id
-	 **/
-	private $adzoneId;
-	
-	/** 
-	 * 后台类目ID，用,分割，最大10个，该ID可以通过taobao.itemcats.get接口获取到
-	 **/
-	private $cat;
-	
-	/** 
-	 * 设备号加密类型：MD5
-	 **/
-	private $deviceEncrypt;
-	
-	/** 
-	 * 设备号类型：IMEI，或者IDFA，或者UTDID
-	 **/
-	private $deviceType;
-	
-	/** 
-	 * 设备号加密后的值
-	 **/
-	private $deviceValue;
-	
-	/** 
-	 * KA媒体淘客佣金比率上限，如：1234表示12.34%
-	 **/
-	private $endKaTkRate;
-	
-	/** 
-	 * 折扣价范围上限，单位：元
-	 **/
-	private $endPrice;
-	
-	/** 
-	 * 淘客佣金比率上限，如：1234表示12.34%
-	 **/
-	private $endTkRate;
-	
-	/** 
-	 * 是否有优惠券，设置为true表示该商品有优惠券，设置为false或不设置表示不判断这个属性
-	 **/
-	private $hasCoupon;
-	
-	/** 
-	 * 好评率是否高于行业均值
-	 **/
-	private $includeGoodRate;
-	
-	/** 
-	 * 成交转化是否高于行业均值
-	 **/
-	private $includePayRate30;
-	
-	/** 
-	 * 退款率是否低于行业均值
-	 **/
-	private $includeRfdRate;
-	
-	/** 
-	 * ip参数影响邮费获取，如果不传或者传入不准确，邮费无法精准提供
-	 **/
-	private $ip;
-	
-	/** 
-	 * 是否海外商品，设置为true表示该商品是属于海外商品，设置为false或不设置表示不判断这个属性
-	 **/
-	private $isOverseas;
-	
-	/** 
-	 * 是否商城商品，设置为true表示该商品是属于淘宝商城商品，设置为false或不设置表示不判断这个属性
-	 **/
-	private $isTmall;
-	
-	/** 
-	 * 所在地
-	 **/
-	private $itemloc;
-	
-	/** 
-	 * 官方的物料Id(详细物料id见：https://tbk.bbs.taobao.com/detail.html?appId=45301&postId=8576096)，不传时默认为2836
-	 **/
-	private $materialId;
-	
-	/** 
-	 * 是否包邮，true表示包邮，空或false表示不限
-	 **/
-	private $needFreeShipment;
-	
-	/** 
-	 * 是否加入消费者保障，true表示加入，空或false表示不限
-	 **/
-	private $needPrepay;
-	
-	/** 
-	 * 牛皮癣程度，取值：1:不限，2:无，3:轻微
-	 **/
-	private $npxLevel;
-	
-	/** 
-	 * 第几页，默认：１
-	 **/
-	private $pageNo;
-	
-	/** 
-	 * 页大小，默认20，1~100
-	 **/
-	private $pageSize;
-	
-	/** 
-	 * 链接形式：1：PC，2：无线，默认：１
-	 **/
-	private $platform;
-	
-	/** 
-	 * 查询词
-	 **/
-	private $q;
-	
-	/** 
-	 * site_id
-	 **/
-	private $siteId;
-	
-	/** 
-	 * 排序_des（降序），排序_asc（升序），销量（total_sales），淘客佣金比率（tk_rate）， 累计推广量（tk_total_sales），总支出佣金（tk_total_commi），价格（price）
-	 **/
-	private $sort;
-	
-	/** 
-	 * 店铺dsr评分，筛选高于等于当前设置的店铺dsr评分的商品0-50000之间
-	 **/
-	private $startDsr;
-	
-	/** 
-	 * KA媒体淘客佣金比率下限，如：1234表示12.34%
-	 **/
-	private $startKaTkRate;
-	
-	/** 
-	 * 折扣价范围下限，单位：元
-	 **/
-	private $startPrice;
-	
-	/** 
-	 * 淘客佣金比率下限，如：1234表示12.34%
-	 **/
-	private $startTkRate;
-	
-	private $apiParas = array();
-	
-	public function setAdzoneId($adzoneId)
-	{
-		$this->adzoneId = $adzoneId;
-		$this->apiParas["adzone_id"] = $adzoneId;
-	}
+    /**
+     * mm_xxx_xxx_12345678三段式的最后一段数字
+     **/
+    private $adzoneId;
 
-	public function getAdzoneId()
-	{
-		return $this->adzoneId;
-	}
+    /**
+     * 商品筛选-后台类目ID。用,分割，最大10个，该ID可以通过taobao.itemcats.get接口获取到
+     **/
+    private $cat;
 
-	public function setCat($cat)
-	{
-		$this->cat = $cat;
-		$this->apiParas["cat"] = $cat;
-	}
+    /**
+     * 智能匹配-设备号加密类型：MD5
+     **/
+    private $deviceEncrypt;
 
-	public function getCat()
-	{
-		return $this->cat;
-	}
+    /**
+     * 智能匹配-设备号类型：IMEI，或者IDFA，或者UTDID（UTDID不支持MD5加密）
+     **/
+    private $deviceType;
 
-	public function setDeviceEncrypt($deviceEncrypt)
-	{
-		$this->deviceEncrypt = $deviceEncrypt;
-		$this->apiParas["device_encrypt"] = $deviceEncrypt;
-	}
+    /**
+     * 智能匹配-设备号加密后的值（MD5加密需32位小写）
+     **/
+    private $deviceValue;
 
-	public function getDeviceEncrypt()
-	{
-		return $this->deviceEncrypt;
-	}
+    /**
+     * 商品筛选-KA媒体淘客佣金比率上限。如：1234表示12.34%
+     **/
+    private $endKaTkRate;
 
-	public function setDeviceType($deviceType)
-	{
-		$this->deviceType = $deviceType;
-		$this->apiParas["device_type"] = $deviceType;
-	}
+    /**
+     * 商品筛选-折扣价范围上限。单位：元
+     **/
+    private $endPrice;
 
-	public function getDeviceType()
-	{
-		return $this->deviceType;
-	}
+    /**
+     * 商品筛选-淘客佣金比率上限。如：1234表示12.34%
+     **/
+    private $endTkRate;
 
-	public function setDeviceValue($deviceValue)
-	{
-		$this->deviceValue = $deviceValue;
-		$this->apiParas["device_value"] = $deviceValue;
-	}
+    /**
+     * 优惠券筛选-是否有优惠券。true表示该商品有优惠券，false或不设置表示不限
+     **/
+    private $hasCoupon;
 
-	public function getDeviceValue()
-	{
-		return $this->deviceValue;
-	}
+    /**
+     * 商品筛选-好评率是否高于行业均值。True表示大于等于，false或不设置表示不限
+     **/
+    private $includeGoodRate;
 
-	public function setEndKaTkRate($endKaTkRate)
-	{
-		$this->endKaTkRate = $endKaTkRate;
-		$this->apiParas["end_ka_tk_rate"] = $endKaTkRate;
-	}
+    /**
+     * 商品筛选(特定媒体支持)-成交转化是否高于行业均值。True表示大于等于，false或不设置表示不限
+     **/
+    private $includePayRate30;
 
-	public function getEndKaTkRate()
-	{
-		return $this->endKaTkRate;
-	}
+    /**
+     * 商品筛选(特定媒体支持)-退款率是否低于行业均值。True表示大于等于，false或不设置表示不限
+     **/
+    private $includeRfdRate;
 
-	public function setEndPrice($endPrice)
-	{
-		$this->endPrice = $endPrice;
-		$this->apiParas["end_price"] = $endPrice;
-	}
+    /**
+     * ip参数影响邮费获取，如果不传或者传入不准确，邮费无法精准提供
+     **/
+    private $ip;
 
-	public function getEndPrice()
-	{
-		return $this->endPrice;
-	}
+    /**
+     * 商品筛选-是否海外商品。true表示属于海外商品，false或不设置表示不限
+     **/
+    private $isOverseas;
 
-	public function setEndTkRate($endTkRate)
-	{
-		$this->endTkRate = $endTkRate;
-		$this->apiParas["end_tk_rate"] = $endTkRate;
-	}
+    /**
+     * 商品筛选-是否天猫商品。true表示属于天猫商品，false或不设置表示不限
+     **/
+    private $isTmall;
 
-	public function getEndTkRate()
-	{
-		return $this->endTkRate;
-	}
+    /**
+     * 商品筛选-所在地
+     **/
+    private $itemloc;
 
-	public function setHasCoupon($hasCoupon)
-	{
-		$this->hasCoupon = $hasCoupon;
-		$this->apiParas["has_coupon"] = $hasCoupon;
-	}
+    /**
+     * 物料id(详细物料id见：https://tbk.bbs.taobao.com/detail.html?appId=45301&postId=8576096)。不传时默认为2836
+     **/
+    private $materialId;
 
-	public function getHasCoupon()
-	{
-		return $this->hasCoupon;
-	}
+    /**
+     * 商品筛选-是否包邮。true表示包邮，false或不设置表示不限
+     **/
+    private $needFreeShipment;
 
-	public function setIncludeGoodRate($includeGoodRate)
-	{
-		$this->includeGoodRate = $includeGoodRate;
-		$this->apiParas["include_good_rate"] = $includeGoodRate;
-	}
+    /**
+     * 商品筛选-是否加入消费者保障。true表示加入，false或不设置表示不限
+     **/
+    private $needPrepay;
 
-	public function getIncludeGoodRate()
-	{
-		return $this->includeGoodRate;
-	}
+    /**
+     * 商品筛选-牛皮癣程度。取值：1不限，2无，3轻微
+     **/
+    private $npxLevel;
 
-	public function setIncludePayRate30($includePayRate30)
-	{
-		$this->includePayRate30 = $includePayRate30;
-		$this->apiParas["include_pay_rate_30"] = $includePayRate30;
-	}
+    /**
+     * 第几页，默认：１
+     **/
+    private $pageNo;
 
-	public function getIncludePayRate30()
-	{
-		return $this->includePayRate30;
-	}
+    /**
+     * 页大小，默认20，1~100
+     **/
+    private $pageSize;
 
-	public function setIncludeRfdRate($includeRfdRate)
-	{
-		$this->includeRfdRate = $includeRfdRate;
-		$this->apiParas["include_rfd_rate"] = $includeRfdRate;
-	}
+    /**
+     * 链接形式：1：PC，2：无线，默认：１
+     **/
+    private $platform;
 
-	public function getIncludeRfdRate()
-	{
-		return $this->includeRfdRate;
-	}
+    /**
+     * 查询的关键词
+     **/
+    private $q;
 
-	public function setIp($ip)
-	{
-		$this->ip = $ip;
-		$this->apiParas["ip"] = $ip;
-	}
+    /**
+     * mm_xxx_22_xxx三段式的第二段数字
+     **/
+    private $siteId;
 
-	public function getIp()
-	{
-		return $this->ip;
-	}
+    /**
+     * 排序_des（降序），排序_asc（升序），销量（total_sales），淘客佣金比率（tk_rate）， 累计推广量（tk_total_sales），总支出佣金（tk_total_commi），价格（price）
+     **/
+    private $sort;
 
-	public function setIsOverseas($isOverseas)
-	{
-		$this->isOverseas = $isOverseas;
-		$this->apiParas["is_overseas"] = $isOverseas;
-	}
+    /**
+     * 商品筛选(特定媒体支持)-店铺dsr评分。筛选大于等于当前设置的店铺dsr评分的商品0-50000之间
+     **/
+    private $startDsr;
 
-	public function getIsOverseas()
-	{
-		return $this->isOverseas;
-	}
+    /**
+     * 商品筛选-KA媒体淘客佣金比率下限。如：1234表示12.34%
+     **/
+    private $startKaTkRate;
 
-	public function setIsTmall($isTmall)
-	{
-		$this->isTmall = $isTmall;
-		$this->apiParas["is_tmall"] = $isTmall;
-	}
+    /**
+     * 商品筛选-折扣价范围上限。单位：元
+     **/
+    private $startPrice;
 
-	public function getIsTmall()
-	{
-		return $this->isTmall;
-	}
+    /**
+     * 商品筛选-淘客佣金比率下限。如：1234表示12.34%
+     **/
+    private $startTkRate;
 
-	public function setItemloc($itemloc)
-	{
-		$this->itemloc = $itemloc;
-		$this->apiParas["itemloc"] = $itemloc;
-	}
+    private $apiParas = array();
 
-	public function getItemloc()
-	{
-		return $this->itemloc;
-	}
+    public function setAdzoneId($adzoneId)
+    {
+        $this->adzoneId = $adzoneId;
+        $this->apiParas["adzone_id"] = $adzoneId;
+    }
 
-	public function setMaterialId($materialId)
-	{
-		$this->materialId = $materialId;
-		$this->apiParas["material_id"] = $materialId;
-	}
+    public function getAdzoneId()
+    {
+        return $this->adzoneId;
+    }
 
-	public function getMaterialId()
-	{
-		return $this->materialId;
-	}
+    public function setCat($cat)
+    {
+        $this->cat = $cat;
+        $this->apiParas["cat"] = $cat;
+    }
 
-	public function setNeedFreeShipment($needFreeShipment)
-	{
-		$this->needFreeShipment = $needFreeShipment;
-		$this->apiParas["need_free_shipment"] = $needFreeShipment;
-	}
+    public function getCat()
+    {
+        return $this->cat;
+    }
 
-	public function getNeedFreeShipment()
-	{
-		return $this->needFreeShipment;
-	}
+    public function setDeviceEncrypt($deviceEncrypt)
+    {
+        $this->deviceEncrypt = $deviceEncrypt;
+        $this->apiParas["device_encrypt"] = $deviceEncrypt;
+    }
 
-	public function setNeedPrepay($needPrepay)
-	{
-		$this->needPrepay = $needPrepay;
-		$this->apiParas["need_prepay"] = $needPrepay;
-	}
+    public function getDeviceEncrypt()
+    {
+        return $this->deviceEncrypt;
+    }
 
-	public function getNeedPrepay()
-	{
-		return $this->needPrepay;
-	}
+    public function setDeviceType($deviceType)
+    {
+        $this->deviceType = $deviceType;
+        $this->apiParas["device_type"] = $deviceType;
+    }
 
-	public function setNpxLevel($npxLevel)
-	{
-		$this->npxLevel = $npxLevel;
-		$this->apiParas["npx_level"] = $npxLevel;
-	}
+    public function getDeviceType()
+    {
+        return $this->deviceType;
+    }
 
-	public function getNpxLevel()
-	{
-		return $this->npxLevel;
-	}
+    public function setDeviceValue($deviceValue)
+    {
+        $this->deviceValue = $deviceValue;
+        $this->apiParas["device_value"] = $deviceValue;
+    }
 
-	public function setPageNo($pageNo)
-	{
-		$this->pageNo = $pageNo;
-		$this->apiParas["page_no"] = $pageNo;
-	}
+    public function getDeviceValue()
+    {
+        return $this->deviceValue;
+    }
 
-	public function getPageNo()
-	{
-		return $this->pageNo;
-	}
+    public function setEndKaTkRate($endKaTkRate)
+    {
+        $this->endKaTkRate = $endKaTkRate;
+        $this->apiParas["end_ka_tk_rate"] = $endKaTkRate;
+    }
 
-	public function setPageSize($pageSize)
-	{
-		$this->pageSize = $pageSize;
-		$this->apiParas["page_size"] = $pageSize;
-	}
+    public function getEndKaTkRate()
+    {
+        return $this->endKaTkRate;
+    }
 
-	public function getPageSize()
-	{
-		return $this->pageSize;
-	}
+    public function setEndPrice($endPrice)
+    {
+        $this->endPrice = $endPrice;
+        $this->apiParas["end_price"] = $endPrice;
+    }
 
-	public function setPlatform($platform)
-	{
-		$this->platform = $platform;
-		$this->apiParas["platform"] = $platform;
-	}
+    public function getEndPrice()
+    {
+        return $this->endPrice;
+    }
 
-	public function getPlatform()
-	{
-		return $this->platform;
-	}
+    public function setEndTkRate($endTkRate)
+    {
+        $this->endTkRate = $endTkRate;
+        $this->apiParas["end_tk_rate"] = $endTkRate;
+    }
 
-	public function setQ($q)
-	{
-		$this->q = $q;
-		$this->apiParas["q"] = $q;
-	}
+    public function getEndTkRate()
+    {
+        return $this->endTkRate;
+    }
 
-	public function getQ()
-	{
-		return $this->q;
-	}
+    public function setHasCoupon($hasCoupon)
+    {
+        $this->hasCoupon = $hasCoupon;
+        $this->apiParas["has_coupon"] = $hasCoupon;
+    }
 
-	public function setSiteId($siteId)
-	{
-		$this->siteId = $siteId;
-		$this->apiParas["site_id"] = $siteId;
-	}
+    public function getHasCoupon()
+    {
+        return $this->hasCoupon;
+    }
 
-	public function getSiteId()
-	{
-		return $this->siteId;
-	}
+    public function setIncludeGoodRate($includeGoodRate)
+    {
+        $this->includeGoodRate = $includeGoodRate;
+        $this->apiParas["include_good_rate"] = $includeGoodRate;
+    }
 
-	public function setSort($sort)
-	{
-		$this->sort = $sort;
-		$this->apiParas["sort"] = $sort;
-	}
+    public function getIncludeGoodRate()
+    {
+        return $this->includeGoodRate;
+    }
 
-	public function getSort()
-	{
-		return $this->sort;
-	}
+    public function setIncludePayRate30($includePayRate30)
+    {
+        $this->includePayRate30 = $includePayRate30;
+        $this->apiParas["include_pay_rate_30"] = $includePayRate30;
+    }
 
-	public function setStartDsr($startDsr)
-	{
-		$this->startDsr = $startDsr;
-		$this->apiParas["start_dsr"] = $startDsr;
-	}
+    public function getIncludePayRate30()
+    {
+        return $this->includePayRate30;
+    }
 
-	public function getStartDsr()
-	{
-		return $this->startDsr;
-	}
+    public function setIncludeRfdRate($includeRfdRate)
+    {
+        $this->includeRfdRate = $includeRfdRate;
+        $this->apiParas["include_rfd_rate"] = $includeRfdRate;
+    }
 
-	public function setStartKaTkRate($startKaTkRate)
-	{
-		$this->startKaTkRate = $startKaTkRate;
-		$this->apiParas["start_ka_tk_rate"] = $startKaTkRate;
-	}
+    public function getIncludeRfdRate()
+    {
+        return $this->includeRfdRate;
+    }
 
-	public function getStartKaTkRate()
-	{
-		return $this->startKaTkRate;
-	}
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+        $this->apiParas["ip"] = $ip;
+    }
 
-	public function setStartPrice($startPrice)
-	{
-		$this->startPrice = $startPrice;
-		$this->apiParas["start_price"] = $startPrice;
-	}
+    public function getIp()
+    {
+        return $this->ip;
+    }
 
-	public function getStartPrice()
-	{
-		return $this->startPrice;
-	}
+    public function setIsOverseas($isOverseas)
+    {
+        $this->isOverseas = $isOverseas;
+        $this->apiParas["is_overseas"] = $isOverseas;
+    }
 
-	public function setStartTkRate($startTkRate)
-	{
-		$this->startTkRate = $startTkRate;
-		$this->apiParas["start_tk_rate"] = $startTkRate;
-	}
+    public function getIsOverseas()
+    {
+        return $this->isOverseas;
+    }
 
-	public function getStartTkRate()
-	{
-		return $this->startTkRate;
-	}
+    public function setIsTmall($isTmall)
+    {
+        $this->isTmall = $isTmall;
+        $this->apiParas["is_tmall"] = $isTmall;
+    }
 
-	public function getApiMethodName()
-	{
-		return "taobao.tbk.sc.material.optional";
-	}
-	
-	public function getApiParas()
-	{
-		return $this->apiParas;
-	}
-	
-	public function check()
-	{
-		
-		RequestCheckUtil::checkNotNull($this->adzoneId,"adzoneId");
-		RequestCheckUtil::checkNotNull($this->siteId,"siteId");
-		RequestCheckUtil::checkMaxValue($this->startDsr,50000,"startDsr");
-	}
-	
-	public function putOtherTextParam($key, $value) {
-		$this->apiParas[$key] = $value;
-		$this->$key = $value;
-	}
+    public function getIsTmall()
+    {
+        return $this->isTmall;
+    }
+
+    public function setItemloc($itemloc)
+    {
+        $this->itemloc = $itemloc;
+        $this->apiParas["itemloc"] = $itemloc;
+    }
+
+    public function getItemloc()
+    {
+        return $this->itemloc;
+    }
+
+    public function setMaterialId($materialId)
+    {
+        $this->materialId = $materialId;
+        $this->apiParas["material_id"] = $materialId;
+    }
+
+    public function getMaterialId()
+    {
+        return $this->materialId;
+    }
+
+    public function setNeedFreeShipment($needFreeShipment)
+    {
+        $this->needFreeShipment = $needFreeShipment;
+        $this->apiParas["need_free_shipment"] = $needFreeShipment;
+    }
+
+    public function getNeedFreeShipment()
+    {
+        return $this->needFreeShipment;
+    }
+
+    public function setNeedPrepay($needPrepay)
+    {
+        $this->needPrepay = $needPrepay;
+        $this->apiParas["need_prepay"] = $needPrepay;
+    }
+
+    public function getNeedPrepay()
+    {
+        return $this->needPrepay;
+    }
+
+    public function setNpxLevel($npxLevel)
+    {
+        $this->npxLevel = $npxLevel;
+        $this->apiParas["npx_level"] = $npxLevel;
+    }
+
+    public function getNpxLevel()
+    {
+        return $this->npxLevel;
+    }
+
+    public function setPageNo($pageNo)
+    {
+        $this->pageNo = $pageNo;
+        $this->apiParas["page_no"] = $pageNo;
+    }
+
+    public function getPageNo()
+    {
+        return $this->pageNo;
+    }
+
+    public function setPageSize($pageSize)
+    {
+        $this->pageSize = $pageSize;
+        $this->apiParas["page_size"] = $pageSize;
+    }
+
+    public function getPageSize()
+    {
+        return $this->pageSize;
+    }
+
+    public function setPlatform($platform)
+    {
+        $this->platform = $platform;
+        $this->apiParas["platform"] = $platform;
+    }
+
+    public function getPlatform()
+    {
+        return $this->platform;
+    }
+
+    public function setQ($q)
+    {
+        $this->q = $q;
+        $this->apiParas["q"] = $q;
+    }
+
+    public function getQ()
+    {
+        return $this->q;
+    }
+
+    public function setSiteId($siteId)
+    {
+        $this->siteId = $siteId;
+        $this->apiParas["site_id"] = $siteId;
+    }
+
+    public function getSiteId()
+    {
+        return $this->siteId;
+    }
+
+    public function setSort($sort)
+    {
+        $this->sort = $sort;
+        $this->apiParas["sort"] = $sort;
+    }
+
+    public function getSort()
+    {
+        return $this->sort;
+    }
+
+    public function setStartDsr($startDsr)
+    {
+        $this->startDsr = $startDsr;
+        $this->apiParas["start_dsr"] = $startDsr;
+    }
+
+    public function getStartDsr()
+    {
+        return $this->startDsr;
+    }
+
+    public function setStartKaTkRate($startKaTkRate)
+    {
+        $this->startKaTkRate = $startKaTkRate;
+        $this->apiParas["start_ka_tk_rate"] = $startKaTkRate;
+    }
+
+    public function getStartKaTkRate()
+    {
+        return $this->startKaTkRate;
+    }
+
+    public function setStartPrice($startPrice)
+    {
+        $this->startPrice = $startPrice;
+        $this->apiParas["start_price"] = $startPrice;
+    }
+
+    public function getStartPrice()
+    {
+        return $this->startPrice;
+    }
+
+    public function setStartTkRate($startTkRate)
+    {
+        $this->startTkRate = $startTkRate;
+        $this->apiParas["start_tk_rate"] = $startTkRate;
+    }
+
+    public function getStartTkRate()
+    {
+        return $this->startTkRate;
+    }
+
+    public function getApiMethodName()
+    {
+        return "taobao.tbk.sc.material.optional";
+    }
+
+    public function getApiParas()
+    {
+        return $this->apiParas;
+    }
+
+    public function check()
+    {
+
+        RequestCheckUtil::checkNotNull($this->adzoneId, "adzoneId");
+        RequestCheckUtil::checkNotNull($this->siteId, "siteId");
+        RequestCheckUtil::checkMaxValue($this->startDsr, 50000, "startDsr");
+    }
+
+    public function putOtherTextParam($key, $value)
+    {
+        $this->apiParas[$key] = $value;
+        $this->$key = $value;
+    }
 }
